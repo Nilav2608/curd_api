224a155d024b0c9168ee05bd782f4565242bff77
Nilavarasu2608 <knlsu444@gmail.com>
2023-04-17 14:01:24 +0530

    button

---
+++ lib/button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class AppButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final String method;
+  final String operation;
+  final Color? methodColor;
+  const AppButton({super.key,required this.operation, required this.methodColor, required this.method, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: onPressed,
+      color: Colors.white,
+      height: 60,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Row(
+        children: [
+          Text(
+            method,
+            style: TextStyle(
+              color: methodColor,
+              fontSize: 32,
+              fontWeight: FontWeight.w900
+            ),
+          ),
+          Text(
+            operation,
+            style: const TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w900
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
